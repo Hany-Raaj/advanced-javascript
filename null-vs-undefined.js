a7aebb1e9a755b178ba1eaa83197042286508e1d
Hany Raaj <hanyraaj@gmail.com>
2021-02-02 22:19:57 +0600

    true false value

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+// undefined
+let car;
+console.log(car);
+
+function add(num1, num2){
+    // console.log(num1 + num2);
+    return
+}
+
+const smartBoy = {
+    name: 'smart dude',
+    phone: 2332432
+}
+
+console.log(smartBoy.age); // undefined
+
+const result = add(33, 45);
+console.log(result);
+
+let fun = undefined;
+console.log(fun);
