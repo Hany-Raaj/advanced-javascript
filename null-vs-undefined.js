4ee0948715028e1394fc9fdbe87f4ce7b5400469
Hany Raaj <hanyraaj@gmail.com>
2021-02-02 22:33:42 +0600

    null vs undefined

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -2,7 +2,7 @@
 let car;
 console.log(car);
 
-function add(num1, num2){
+function add(num1, num2) {
     // console.log(num1 + num2);
     return
 }
@@ -19,3 +19,8 @@ console.log(result);
 
 let fun = undefined;
 console.log(fun);
+
+function doSomething(x, y) {
+    console.log(y);
+}
+doSomething(32);
